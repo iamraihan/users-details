fdf655e4250c0f4a1412ae79adc677a03010521b
iamraihan <trueraihan@gmail.com>
2022-09-03 23:24:58 +0600

    update code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const personRouter = require("./routes/personRouter");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +20,6 @@ app.all("*", (req, res) => {
   res.send("No Route Found!!");
 });
 
-app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
 });
