5c1758ef75c2ba6fc4d3107bb36a5b45d3537c74
iamraihan <trueraihan@gmail.com>
2022-09-03 19:50:49 +0600

    limit set

--- controllers/personController.js
+++ controllers/personController.js
@@ -1,13 +1,12 @@
 const data = {};
 data.person = require("../models/personModel");
 module.exports.getAllPerson = (req, res, next) => {
-  res.json(data.person);
-  console.log(data.person);
+  const { limit } = req.query;
+  res.json(data.person.slice(10, limit));
 };
 
 module.exports.getRandomUser = (req, res, next) => {
   const allUser = data.person;
-
   const user = allUser[Math.floor(Math.random() * allUser.length)];
   res.json(user);
 };
--- routes/personRouter.js
+++ routes/personRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const personController = require("../controllers/personController");
 const router = express.Router();
 
-router.route("/").get(personController.getAllPerson);
+router.route("/all").get(personController.getAllPerson);
 router.route("/random").get(personController.getRandomUser);
 
 module.exports = router;
